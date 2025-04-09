5fb7ec4b1a06b58d9adb86c409f097ba8e6e99ad
Collin Schwantes <cjschwantesll@gmail.com>
2025-04-09 11:04:20 -0400

    Update project_startup.R

--- scripts/project_startup.R
+++ scripts/project_startup.R
@@ -14,6 +14,13 @@ usethis::use_mit_license(copyright_holder = Sys.getenv("copyright_holder"))
 ## setup renv for package management
 renv::init()
 
+# use renv::hydrate to get any packages already installed on your machine
+#renv::hydrate()
+# use renv::status to see if any packages need to be installed
+#renv::status()
+# use renv::install to install missing packages
+#renv::install("openssl")
+
 # At some point `renv` will cause you a headache. 
 # Its a good idea to talk to collaborators about how you will manage
 # the lock file.
